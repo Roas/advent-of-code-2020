561a9879137f8a6fcf64471f4e71825e56405d1e
Sander Roosingh <sjroosingh@gmail.com>
2020-12-22 00:52:57 +0100

    Day 9 part one code

--- day9/part_one.py
+++ day9/part_one.py
@@ -2,10 +2,33 @@
 
 
 INPUT_FILE = "input.txt"
+PREAMBLE = 25
 
 
 def main():
     content = read_input()
+    number = process_content(content)
+    print(number)
+
+
+def process_content(content):
+    i = PREAMBLE
+    while i < len(content):
+        options = content[i-PREAMBLE:i]
+        options_sum = calculate_all_options(options)
+        if int(content[i]) not in options_sum:
+            return content[i]
+        i = i + 1
+    return "Fuck..."
+
+
+def calculate_all_options(options):
+    foo = []
+    for option in options:
+        for option2 in options:
+            if option != option2:
+                foo.append(int(option) + int(option2))
+    return foo
 
 
 def read_input():
